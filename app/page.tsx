beab8edc54f138b1d143cef2f3f210a23a848757
Denis Pop <pop.denis1902@gmail.com>
2023-09-03 23:20:32 +0300

    use the Link, Script and Image from next

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
 import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <h1 className="title">
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="title"></h1>
       Read <Link href="/posts/first-post">this page!</Link>
-    </h1>
+      <Image
+        src="/images/Ayrton_Senna_1988_Canada.webp"
+        width={500}
+        height={500}
+        alt="Ayrton Senna 1988"
+      />
+    </div>
   );
 }
--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,10 +1,23 @@
 import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
-    <>
+    <div>
+      <Head>
+        <title>Random page</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+
       <h1>Random page</h1>
       <Link href="/">Back to home</Link>
-    </>
+    </div>
   );
 }
